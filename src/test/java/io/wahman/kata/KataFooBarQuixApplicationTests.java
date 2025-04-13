0f67e15ad5f676f74cadee25cc9e69910b81d1e9
Abdelilah Wahman <abdelilah.wahman@capgemini.com>
2025-04-13 15:19:14 +0200

    initial commit

---
+++ src/test/java/io/wahman/kata/KataFooBarQuixApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wahman.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataFooBarQuixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

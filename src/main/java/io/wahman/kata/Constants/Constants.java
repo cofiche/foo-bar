c850d1f46e935f931cc541f321e8879c3eec0d51
Abdelilah Wahman <abdelilah.wahman@capgemini.com>
2025-04-14 14:11:38 +0200

    remove constants

--- src/main/java/io/wahman/kata/Constants/Constants.java
+++ src/main/java/io/wahman/kata/Constants/Constants.java
@@ -4,8 +4,5 @@ public class Constants {
 
     public static final String JOB_NAME = "FooBarJob";
     public static final String STEP_NAME = "FooBarStep";
-    public static final String INPUT_PATH = "src/main/resources/input/inputFile.txt";
-    public static final String OUTPUT_PATH = "src/main/resources/output/outputFile.txt";
-
 
 }
